59d51209b0145706fcc1eefda85853b3afe0c422
Ryt666 <96390850+Ryt666@users.noreply.github.com>
2021-12-20 11:47:30 +0300

    Create VagonType.cs

---
+++ VagonType.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._2
+{
+    public enum WagonType
+    {
+        Double = 0,
+        Soft,
+        Compartmen,
+        Second_class,
+        General,
+        Sedentary
+    }
+}
