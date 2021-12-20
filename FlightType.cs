34683897f926efb2c349857777dd846ae1251b00
Ryt666 <96390850+Ryt666@users.noreply.github.com>
2021-12-20 11:50:56 +0300

    Create FlightType.cs

---
+++ FlightType.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._2
+{
+    public enum FlightType
+    {
+        International = 0,
+        Local
+    }
+}
